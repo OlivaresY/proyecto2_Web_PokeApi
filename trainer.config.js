765ebbfe4bfb7711a2f93c9f48afe806e8fa008f
OlivaresY <124736762+OlivaresY@users.noreply.github.com>
2026-04-07 18:00:28 -0600

    Creacion de trainer.config para cargar datos del entrenador y pokemon favorito

---
+++ trainer.config.js
@@ -0,0 +1,6 @@
+export default {
+  name: "Nombre: YOR",                
+  hometown: "Pueblo: PZ",             
+  catchphrase: "Frase: ¡Confío en mis Pokémon!", 
+  favoritePokemon: "golduck"  
+};
